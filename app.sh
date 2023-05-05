9f7ac50fa270ad3581711c87746e740997486e93
mickeydarrenlau <darrenwjlau@gmail.com>
2023-05-05 16:43:01 +0800

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,18 +1,18 @@
 apt install --yes --force-yes curl 
-if [ -d "/data/" ] 
+if [ -d "/app/" ] 
 then
-    echo "Directory /data/ exists." 
-    PATH="/data/bin:$PATH"
-    code-server --config /data/config &
-    cat /data/config/config.yml
+    echo "Directory /app/ exists." 
+    PATH="/app/bin:$PATH"
+    code-server --config /app/config &
+    cat /app/config/config.yml
 else
-    echo "Error: Directory /data/ does not exists."
-    mkdir -p /data/lib /data/bin
+    echo "Error: Directory /app/ does not exists."
+    mkdir -p /app/lib /app/bin
     curl -fL https://github.com/coder/code-server/releases/download/v$VERSION/code-server-$VERSION-linux-amd64.tar.gz \
-      | tar -C /data/lib -xz
-    mv /data/code-server-$VERSION-linux-amd64 /data/lib/code-server-$VERSION
-    ln -s /data/code-server-$VERSION/bin/code-server /data/bin/code-server
-    PATH="/data/bin:$PATH"
-    code-server --config /data/config &
-    cat /data/config/config.yml
+      | tar -C /app/lib -xz
+    mv /app/code-server-$VERSION-linux-amd64 /app/lib/code-server-$VERSION
+    ln -s /app/code-server-$VERSION/bin/code-server /app/bin/code-server
+    PATH="/app/bin:$PATH"
+    code-server --config /app/config &
+    cat /app/config/config.yml
 fi
