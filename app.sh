7bcf4b663ec62e1241bea33710893f4ef3a10ba5
mickeydarrenlau <darrenwjlau@gmail.com>
2023-05-05 16:52:31 +0800

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,19 +1,19 @@
 VERSION = "4.12.0"
 apt install --yes --force-yes curl 
-if [ -d "/app/bin" ] 
+if [ -d "/app/coder/bin" ] 
 then
     echo "Directory /app/bin exists." 
-    PATH="/app/bin:$PATH"
-    code-server --config /app/config &
-    cat /app/config/config.yml
+    PATH="/app/coder/bin:$PATH"
+    code-server --config /app/coder/config &
+    cat /app/coder/config/config.yml
 else
     echo "Error: Directory /app/bin does not exists."
-    mkdir -p /app/lib /app/bin
+    mkdir -p /app/coder/lib /app/coder/bin
     curl -fL https://github.com/coder/code-server/releases/download/v$VERSION/code-server-$VERSION-linux-amd64.tar.gz \
-      | tar -C /app/lib -xz
-    mv /app/code-server-$VERSION-linux-amd64 /app/lib/code-server-$VERSION
+      | tar -C /app/coder/lib -xz
+    mv /app/coder/code-server-$VERSION-linux-amd64 /app/coder/lib/code-server-$VERSION
     ln -s /app/code-server-$VERSION/bin/code-server /app/bin/code-server
-    PATH="/app/bin:$PATH"
-    code-server --config /app/config &
-    cat /app/config/config.yml
+    PATH="/app/coder/bin:$PATH"
+    code-server --config /app/coder/config &
+    cat /app/coder/config/config.yml
 fi
