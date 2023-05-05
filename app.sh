4fe6681950679784714f92decadad964d6499fe2
mickeydarrenlau <darrenwjlau@gmail.com>
2023-05-05 16:57:12 +0800

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,19 +1,19 @@
 VERSION = "4.12.0"
 apt install --yes --force-yes curl 
-if [ -d "/app/coder/bin" ] 
+if [ -d "/app/coder2/bin" ] 
 then
-    echo "Directory /app/bin exists." 
-    PATH="/app/coder/bin:$PATH"
-    code-server --config /app/coder/config &
-    cat /app/coder/config/config.yml
+    echo "Directory /app/coder2/bin exists." 
+    PATH="/app/coder2/bin:$PATH"
+    code-server --config /app/coder2/config &
+    cat /app/coder2/config/config.yml
 else
-    echo "Error: Directory /app/bin does not exists."
-    mkdir -p /app/coder/lib /app/coder/bin
+    echo "Error: Directory /app/coder2/bin does not exists."
+    mkdir -p /app/coder2/lib /app/coder2/bin
     curl -fL https://github.com/coder/code-server/releases/download/v$VERSION/code-server-$VERSION-linux-amd64.tar.gz \
-      | tar -C /app/coder/lib -xz
-    mv /app/coder/code-server-$VERSION-linux-amd64 /app/coder/lib/code-server-$VERSION
-    ln -s /app/code-server-$VERSION/bin/code-server /app/bin/code-server
-    PATH="/app/coder/bin:$PATH"
-    code-server --config /app/coder/config &
-    cat /app/coder/config/config.yml
+      | tar -C /app/coder2/lib -xz
+    mv /app/coder2/code-server-$VERSION-linux-amd64 /app/coder2/lib/code-server-$VERSION
+    ln -s /app/coder2/code-server-$VERSION/bin/code-server /app/coder2/bin/code-server
+    PATH="/app/coder2/bin:$PATH"
+    code-server --config /app/coder2/config &
+    cat /app/coder2/config/config.yml
 fi
